3774c821abc247ff82f1f3d912563c769ea99422
AlexThe86 <mr_axel_peter@web.de>
2023-07-13 16:17:53 +0200

    update

--- src/Hero1.kt
+++ src/Hero1.kt
@@ -1,6 +1,13 @@
-class Hero1 : Hero("Krieger", 100, mutableListOf(AttackAction("Angriff", 20), AttackAction("Schwertwirbel", 30), AttackAction("Todesstoß", 40)), Bag().apply{
-    addItem(Item("Heiltrank1", 50, 0))
-    addItem(Item("Heiltrank2", 50, 0))
-    addItem(Item("Heiltrank3", 50, 0))
-    addItem(Item("Vitamine", 0, 30))
-})
+class Hero1 : Hero(
+    //klasse erbt von Hero//
+    "Krieger", 100, listOf(
+        AttackAction("Angriff", 20),
+        AttackAction("Schwertwirbel", 30),
+        AttackAction("Todesstoß", 40),
+    ), Bag().apply {
+        addItem(Item("Heiltrank1", 50,0))
+        addItem(Item("Heiltrank2", 50,0))
+        addItem(Item("Heiltrank3", 50,0))
+        addItem(Item("Vitamine", 0,30))
+    }
+)
--- src/Hero2.kt
+++ src/Hero2.kt
@@ -1,6 +1,13 @@
-class Hero2 : Hero("Magier", 80, mutableListOf(AttackAction("Zauber", 30), AttackAction("Feuerball", 35), AttackAction("Blitzschlag", 40)), Bag().apply{
-    addItem(Item("Heiltrank1", 50, 0))
-    addItem(Item("Heiltrank2", 50, 0))
-    addItem(Item("Heiltrank3", 50, 0))
-    addItem(Item("Vitamine", 0, 30))
-})
+class Hero2 : Hero(
+    //klasse erbt von Hero//
+    "Magier", 80, listOf(
+        AttackAction("Zauber", 30),
+        AttackAction("Feuerball", 35),
+        AttackAction("Blitzschlag", 40),
+    ), Bag().apply {
+        addItem(Item("Heiltrank1", 50,0))
+        addItem(Item("Heiltrank2", 50,0))
+        addItem(Item("Heiltrank3", 50,0))
+        addItem(Item("Vitamine", 0,30))
+    }
+)
