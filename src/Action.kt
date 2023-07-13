46a858baa3a35b7037be4feabb5540d416c0c5be
AlexThe86 <mr_axel_peter@web.de>
2023-07-13 21:29:35 +0200

    update

--- src/Action.kt
+++ src/Action.kt
@@ -3,5 +3,6 @@ und der apply methode die von den unterklassen überschrieben werden kann
 dazu hat die klasse 2 parameter act für Charakter und target für Enemy.*/
 open class Action(open val name: String) {
     open fun apply(act: Charakter, target: Enemy) {
+        //die methode kann von den jeweiligen unterklassen überschrieben werden.//
     }
 }
